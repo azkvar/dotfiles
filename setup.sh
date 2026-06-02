4ded0cbaa70921c52586f3164cee3494baaf9ed3
sumiyuki-sub <285984494+sumiyuki-sub@users.noreply.github.com>
2026-06-02 10:35:10 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-
 brew bundle --file="Brewfile" --no-upgrade
-
 mkdir -p "$HOME/.config/ghostty" "$HOME/.config/zed"
-
 ln -sfn "$PWD/zsh/.zshrc" "$HOME/.zshrc"
 ln -sfn "$PWD/git/.gitconfig" "$HOME/.gitconfig"
 ln -sfn "$PWD/ghostty/config" "$HOME/.config/ghostty/config"
