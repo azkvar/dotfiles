67cdc412384670c6c47cf271a95a4f79879f0540
azkvar <285984494+azkvar@users.noreply.github.com>
2026-06-04 14:33:43 +0900

    fix

--- fish/conf.d/20-abbr.fish
+++ fish/conf.d/20-abbr.fish
@@ -3,5 +3,10 @@ abbr -a -- ls eza
 abbr -a -- ll 'eza -l'
 abbr -a -- la 'eza -la'
 abbr -a -- lt 'eza --tree --level=2'
+abbr -a -- g git
+abbr -a -- c clear
+abbr -a -- mkdir 'mkdir -p'
+abbr -a -- d docker
+abbr -a -- dc 'docker compose'
 abbr -a -- gcc gcc-15
 abbr -a -- g++ g++-15
